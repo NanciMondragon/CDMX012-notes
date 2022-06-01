2e3823d1ee87ee3d77a3e71f29e3e348b7a2fb30
NanciMondragon <nantzamon@gmail.com>
2022-05-31 23:51:54 -0500

    Functionality of the edit note button, performs CRUD

--- write-notes/src/components/Feed/EditNote.jsx
+++ write-notes/src/components/Feed/EditNote.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from '../../lib/firebase';
+import Footer from './Footer';
+
 //import NewNotes from 'first'
 
 
@@ -12,25 +14,26 @@ function EditNote() {
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewDescription] = useState("");
   const { id } = useParams();
-  console.log({id})
+  //console.log({id})
 
   const navigate = useNavigate();
 
+  //actualizar las notas
   const update = async (e) => {
     e.preventDefault()
-   const note =doc (db, "notes", id);
-   const data = { title: newTitle, description: newDescription}
-   console.log(data)
+    const note = doc(db, "notes", id)
+   const data = {title: newTitle, description: newDescription}
    await updateDoc (note, data);
    navigate("/Timeline");
   }
 
+  //traer los datos de las notas
   const getNoteById = async (id) => {
    const note = await getDoc( doc(db, "notes", id) )
    if (note.exists()){
-    //console.log(note.data())
-    setNewDescription(note. data().description)    
-    setNewTitle(note. data().title)
+    console.log(note.data())
+    setNewDescription(note.data().description)    
+    setNewTitle(note.data().title)
    }else{
      console.log('El producto no existe');
    }
@@ -48,7 +51,27 @@ function EditNote() {
 
 
   return (
-    <div>EditNote</div>
+    <>
+    <div> 
+        <header className="title">
+        </header>
+       
+        <form className='general' onSubmit={update}>
+          <input className='titleCss' value = {newTitle}  onChange={(e) => {
+            setNewTitle(e.target.value);
+          }}></input>
+          <textarea className='writeNoteCss' value = {newDescription} onChange={(e) => {
+            setNewDescription(e.target.value); 
+          }}></textarea>
+          <button className='btnCreate' type="submit" > UPDATE </button>
+        </form>
+       
+
+    </div>
+
+    <Footer/>
+
+    </>
   )
 }
 
